73f35a3390c381d2d60c5d150be8aa8b68bfa0ea
Vance Miller <vance@cs.utexas.edu>
2018-08-12 16:53:26 -0500

    tests in a different place

--- test/tests/process.cpp
+++ test/tests/process.cpp
@@ -11,37 +11,37 @@ TEST(Process, SpawnInvalid) {
 }
 
 TEST(Process, ReturnValue) {
-  Process hello({"test/hello_world"});
+  Process hello({"hello_world"});
   EXPECT_EQ(123, hello.wait());
 }
 
 TEST(Process, WaitTwice) {
-  Process hello({"test/hello_world"});
-  hello.wait();
+  Process hello({"hello_world"});
+  EXPECT_EQ(123, hello.wait());
   EXPECT_THROW(hello.wait(), std::runtime_error);
 }
 
 TEST(Process, Kill) {
-  Process hello({"test/hello_world"});
+  Process hello({"hello_world"});
   hello.kill();
-  hello.wait();
+  EXPECT_EQ(SIGKILL, hello.wait());
 }
 
 TEST(Process, KillAfterWait) {
-  Process hello({"test/hello_world"});
+  Process hello({"hello_world"});
   hello.wait();
   EXPECT_THROW(hello.kill(), std::runtime_error);
 }
 
 TEST(Process, InfiniteLoopKill) {
-  Process inf({"test/infinite_loop"});
+  Process inf({"infinite_loop"});
   sleep(1);
   inf.kill(SIGKILL);
   EXPECT_EQ(SIGKILL, inf.wait());
 }
 
 TEST(Process, KillTwice) {
-  Process inf({"test/infinite_loop"});
+  Process inf({"infinite_loop"});
   sleep(1);
   inf.kill(SIGKILL);
   EXPECT_NO_THROW(inf.kill()); // can call kill as much as we want
@@ -49,11 +49,11 @@ TEST(Process, KillTwice) {
 }
 
 TEST(Process, Move) {
-  Process inf({"test/infinite_loop"});
+  Process inf({"infinite_loop"});
   sleep(1);
   Process moved(std::move(inf));
   EXPECT_THROW(inf.kill(), std::runtime_error);
   EXPECT_THROW(inf.wait(), std::runtime_error);
   moved.kill();
-  moved.wait();
+  EXPECT_EQ(SIGKILL, moved.wait());
 }
